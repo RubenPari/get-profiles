3392b9e3cdceb0be9259d7c12b2824bda8b81db5
RubenPari <rubenpari.developer@gmail.com>
2022-12-01 19:32:13 +0100

    completed login and callback method

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,19 +4,44 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Cake\Http\Client;
+
 class AuthController extends AppController
 {
     public function login()
     {
-        $this->set('data', [
-            'message' => 'login',
-        ]);
+        $url = env('BASE_URL') . '/oauth/authorize'
+            . '?client_id=' . env('CLIENT_ID')
+            . '&redirect_uri=' . env('REDIRECT_URI')
+            . '&response_type=code'
+            . '&scope=' . env('SCOPE');
+
+        $this->redirect($url);
     }
 
     public function callback()
     {
-        $this->set('data', [
-            'message' => 'callback',
+        $code = $this->request->getQuery('code');
+
+        $url = env('BASE_URL') . '/oauth/access_token';
+
+        $data = [
+            'client_id' => env('CLIENT_ID'),
+            'client_secret' => env('CLIENT_SECRET'),
+            'grant_type' => 'authorization_code',
+            'redirect_uri' => env('REDIRECT_URI'),
+            'code' => $code,
+        ];
+
+        $http = new Client();
+
+        $response = $http->post($url, $data);
+
+        // save token in session
+        $this->request->getSession()->write('token', $response->getJson()['access_token']);
+
+        $this->set([
+            'data' => $response->getJson(),
         ]);
     }
 }
